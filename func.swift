38c2e41948638192770a46e0dfc1cce77445e25d
Ryutaro <ricken0203@gmail.com>
2017-12-01 20:34:24 +0900

    tour

---
+++ func.swift
@@ -0,0 +1,5 @@
+func greet(name: String, food: String) -> String {
+    return "Hello \(name), today is \(food)."
+}
+
+greet(name: "Bob", food: "Ramen")
---
+++ total.swift
@@ -0,0 +1,6 @@
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+
+print (total)
